b78fda831847ddfa8972ca21f27a3191164345bd
James Cairoli <cairoli91@gmail.com>
2021-08-25 14:00:31 -0400

    End of Section 14

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -110,4 +110,14 @@ import { User } from '../_models/user';
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
 
+  addLike(username: string){
+    return this.http.post(this.baseUrl + 'likes/' + username, {});
+  }
+
+  getLikes(predicate: string, pageNumber, pageSize){
+    let params = this.getPaginationHeaders(pageNumber,pageSize)
+    params = params.append('predicate', predicate);
+    return this.getPaginatedResult<Partial<Member[]>>(this.baseUrl + 'likes/', params);
+  }
+
  }
