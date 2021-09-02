b3593be2f9a6291229b1eb1c839afcb2f47cb395
James Cairoli <cairoli91@gmail.com>
2021-09-02 16:12:50 -0400

    End of Section 17

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Member } from 'src/app/_models/member';
-import { MembersService } from 'src/app/_services/members.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
 import { TabDirective, TabsetComponent } from 'ngx-bootstrap/tabs';
 import { MessageService } from 'src/app/_services/message.service';
 import { Message } from 'src/app/_models/message';
+import { PresenceService } from 'src/app/_services/presence.service';
+import { AccountService } from 'src/app/_services/account.service';
+import { User } from 'src/app/_models/user';
+import { take } from 'rxjs/operators';
 
  @Component({
    selector: 'app-member-detail',
@@ -19,8 +22,13 @@ import { Message } from 'src/app/_models/message';
    galleryImages: NgxGalleryImage[];
    activeTab: TabDirective;
    messages: Message[] = [];
+   user: User;
 
-   constructor(private memberService: MembersService, private messageService: MessageService, private route: ActivatedRoute) { }
+   constructor(public presence: PresenceService, private messageService: MessageService, private route: ActivatedRoute, private accountService: AccountService, private router: Router) { 
+     this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
+     this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+   }
+  
 
    ngOnInit(): void {
      this.route.data.subscribe(data => {
@@ -70,8 +78,14 @@ import { Message } from 'src/app/_models/message';
    onTabActivated(data: TabDirective){
     this.activeTab = data;
     if(this.activeTab.heading === 'Messages' && this.messages.length === 0) {
-      this.loadMessages();
+      this.messageService.createHubConnection(this.user, this.member.userName)
+    } else {
+        this.messageService.stopHubConnection();
     }
    }
+
+   ngOnDestroy(): void {
+    this.messageService.stopHubConnection();
+  }
 
  }
